09c6bb8237a02de2ad6e76e2d939066c89750cc1
Erick Martinez <Erick Martinez>
2017-06-16 13:31:26 -0600

    Main y clases creadas

---
+++ Lab8_Erick_Martinez/src/lab8_erick_martinez/Contacto.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_erick_martinez;
+
+/**
+ *
+ * @author usuario
+ */
+public class Contacto {
+    private String nombre;
+    private int edad;
+    private int numero;
+    private String correo;
+    private String direccion;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, int edad, int numero, String correo, String direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numero = numero;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+}
---
+++ Lab8_Erick_Martinez/src/lab8_erick_martinez/Mensaje.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_erick_martinez;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author usuario
+ */
+public class Mensaje {
+    private Contacto emisor;
+    private Contacto receptor;
+    private Date fecha;
+    private String contenido;
+
+    public Mensaje() {
+    }
+
+    public Mensaje(Contacto emisor, Contacto receptor, Date fecha, String contenido) {
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.fecha = fecha;
+        this.contenido = contenido;
+    }
+
+    public Contacto getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(Contacto emisor) {
+        this.emisor = emisor;
+    }
+
+    public Contacto getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(Contacto receptor) {
+        this.receptor = receptor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    @Override
+    public String toString() {
+        return contenido;
+    }
+    
+}
